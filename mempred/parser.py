19606bc1e01369d1424f936697f56eddcacb382e
raharjaliu <raharjaliu@gmail.com>
2016-05-11 23:49:05 +0200

    added k-grams and map merger routines

--- mempred/parser.py
+++ mempred/parser.py
@@ -44,98 +44,159 @@ def parse_sequence_file(file_path):
     return seq_dict
 
 
+def get_k_gram(strin, k):
+    if k < len(strin):
+
+        return {}
+
+    else:
+
+        kgram = {}
+
+        for i in xrange(len(str) - k):
+
+            this_str = strin[i:(i + k)]
+
+            if this_str not in kgram:
+
+                kgram[this_str] = 1
+
+            else:
+
+                kgram[this_str] += 1
+
+        return kgram
+
+
+
+def get_aa_distrib(seq):
+
+    init_distrib = {'a': 0, 'r': 0, 'n': 0,
+					'd': 0, 'c': 0, 'e': 0,
+					'q': 0, 'g': 0, 'h': 0,
+					'i': 0, 'l': 0, 'k': 0,
+					'm': 0, 'f': 0, 'p': 0,
+					's': 0, 't': 0, 'w': 0,
+					'y': 0, 'v': 0}
+
+    for chr in seq:
+
+		init_distrib[chr] = init_distrib[chr] + 1
+
+    return init_distrib
+
+
+def merge_dict(d1, d2):
+
+    """
+    Merge the values of two dicts.
+    It is strictly assumed that both dicts have identical keyset
+    and the values are of numerical types
+
+    :return: dict with combined values
+
+    :param d1: first dictionary
+    :param d2: second dictionary
+    :return:
+    """
+
+    for key in d2:
+        d1[key] += d2[key]
+
+	return d1
+
+
+
 if __name__ == "__main__":
 
-	# path of this file's directory
-	current_path = os.path.dirname(os.path.realpath(__file__))
+    # path of this file's directory
+    current_path = os.path.dirname(os.path.realpath(__file__))
 
-	# path where the file opm_unmasked_hval0.fasta is located within the project
-	opm_path = current_path + "/../data/sets/opm_unmasked_hval0.fasta"
-	opm_buff = open(opm_path)
+    # path where the file opm_unmasked_hval0.fasta is located within the project
+    opm_path = current_path + "/../data/sets/opm_unmasked_hval0.fasta"
+    opm_buff = open(opm_path)
 
-	# path where the file pdbtm_unmasked_hval0.fastais located within the project
-	pdbtm_path = current_path + "/../data/sets/pdbtm_unmasked_hval0.fasta"
-	pdbtm_buff = open(pdbtm_path)
+    # path where the file pdbtm_unmasked_hval0.fastais located within the project
+    pdbtm_path = current_path + "/../data/sets/pdbtm_unmasked_hval0.fasta"
+    pdbtm_buff = open(pdbtm_path)
 
-	# parse OPM file and represent it as a map
-	opm_seq_dict = parse_sequence_file(opm_path)
+    # parse OPM file and represent it as a map
+    opm_seq_dict = parse_sequence_file(opm_path)
 
+    # parse PDBTM file and represent it as a map
+    pdbtm_sequence_dict = parse_sequence_file(pdbtm_path)
 
-	# parse PDBTM file and represent it as a map
-	pdbtm_sequence_dict = parse_sequence_file(pdbtm_path)
+    # print opm_seq_dict
+    # print pdbtm_sequence_dict
 
-	# print opm_seq_dict
-	# print pdbtm_sequence_dict
+    ctr_opm = 0
+    ctr_pdbtm = 0
 
-	ctr_opm = 0
-	ctr_pdbtm = 0
+    ctr_length_opm = 0
+    ctr_length_pdbtm = 0
 
-	ctr_length_opm = 0
-	ctr_length_pdbtm = 0
+    opm_len_dict = {}
+    pdbtm_len_dict = {}
 
-	opm_len_dict = {}
-	pdbtm_len_dict = {}
+    # variables bla and foo can be ignored. they are unimportant because they are only used as intermediate.
+    bla = ""
+    foo = ""
 
-	# variables bla and foo can be ignored. they are unimportant because they are only used as intermediate.
-	bla = ""
-	foo = ""
+    # species is powidl. species opm dict / species pdbtm dict are the dicts for species and number of occurence.
+    species = ""
+    species_opm_dict = {}
+    species_pdbtm_dict = {}
+    for key in opm_seq_dict:
+        # print key
+        bla = key.split("_")[1]
+        species = bla.split()[0]
+        # print species
+        if (species in species_opm_dict):
+            species_opm_dict[species] += 1
+        else:
+            species_opm_dict[species] = 1
+        # print species_opm_dict
+        bla = opm_seq_dict[key][0]
+        # print bla
+        opm_len_dict[ctr_opm] = len(bla.split(",")[0])
+        # bla = opm_seq_dict.values()[ctr_opm][0]
+        # opm_len_dict[ctr_opm] = len(bla.split(",")[0])
+        ctr_opm += 1
 
-	# species is powidl. species opm dict / species pdbtm dict are the dicts for species and number of occurence.
-	species = ""
-	species_opm_dict = {}
-	species_pdbtm_dict = {}
-	for key in opm_seq_dict:
-		#print key
-		bla = key.split("_")[1]
-		species = bla.split()[0]
-		#print species
-		if(species in species_opm_dict):
-			species_opm_dict[species] += 1
-		else:
-			species_opm_dict[species] = 1
-		#print species_opm_dict
-		bla = opm_seq_dict[key][0]
-		#print bla
-		opm_len_dict[ctr_opm] = len(bla.split(",")[0])
-		#bla = opm_seq_dict.values()[ctr_opm][0]
-		#opm_len_dict[ctr_opm] = len(bla.split(",")[0])
-		ctr_opm += 1
+    # print "-----------++"
 
-	#print "-----------++"
+    for key2 in pdbtm_sequence_dict:
+        bla = key2.split("_")[1]
+        species = bla.split()[0]
+        # print species
+        if (species in species_pdbtm_dict):
+            species_pdbtm_dict[species] += 1
+        else:
+            species_pdbtm_dict[species] = 1
+        bla = pdbtm_sequence_dict[key2][0]
+        # print bla
+        # bla = pdbtm_sequence_dict.values()[ctr_pdbtm][0]
+        pdbtm_len_dict[ctr_pdbtm] = len(bla.split(",")[0])
+        ctr_pdbtm += 1
 
-	for key2 in pdbtm_sequence_dict:
-		bla = key2.split("_")[1]
-		species = bla.split()[0]
-		#print species
-		if(species in species_pdbtm_dict):
-			species_pdbtm_dict[species] += 1
-		else:
-			species_pdbtm_dict[species] = 1
-		bla = pdbtm_sequence_dict[key2][0]
-		#print bla
-		# bla = pdbtm_sequence_dict.values()[ctr_pdbtm][0]
-		pdbtm_len_dict[ctr_pdbtm] = len(bla.split(",")[0])
-		ctr_pdbtm += 1
-
-	"""
+    """
 	Descriptive statistics for the data
 	"""
 
-	print "mean of values in opm database is " + str(np.mean(opm_len_dict.values()))
-	print "median of values in opm database is " + str(np.median(opm_len_dict.values()))
-	print "standard deviation of values in opm database is " + str(np.std(opm_len_dict.values()))
-	print "mean of values in pdbtm database is " + str(np.mean(pdbtm_len_dict.values()))
-	print "median of values in pdbtm database is " + str(np.median(pdbtm_len_dict.values()))
-	print "standard deviation of values in pdbtm database is " + str(np.std(pdbtm_len_dict.values()))
+    print "mean of values in opm database is " + str(np.mean(opm_len_dict.values()))
+    print "median of values in opm database is " + str(np.median(opm_len_dict.values()))
+    print "standard deviation of values in opm database is " + str(np.std(opm_len_dict.values()))
+    print "mean of values in pdbtm database is " + str(np.mean(pdbtm_len_dict.values()))
+    print "median of values in pdbtm database is " + str(np.median(pdbtm_len_dict.values()))
+    print "standard deviation of values in pdbtm database is " + str(np.std(pdbtm_len_dict.values()))
 
-	# plot histogram
-	plt.hist(opm_len_dict.values(),bins=50)
-	plt.title("Sequence lengths in OPM file")
-	plt.xlabel("Length")
-	plt.ylabel("Number of occurences")
-	plt.show()
+    # plot histogram
+    plt.hist(opm_len_dict.values(), bins=50)
+    plt.title("Sequence lengths in OPM file")
+    plt.xlabel("Length")
+    plt.ylabel("Number of occurences")
+    plt.show()
 
-	# print species
-	for i in species_pdbtm_dict:
-		print str(i) + "\t" + str(species_opm_dict[i])
-
+    # print species
+    for i in species_pdbtm_dict:
+        print str(i) + "\t" + str(species_opm_dict[i])
