c9356581cba6286381884953a3560c43c9cc45ed
raharjaliu <raharjaliu@gmail.com>
2016-05-17 01:38:46 +0200

    refactored project. Debugged AA distrib. k-gram also working now

--- mempred/parser.py
+++ mempred/parser.py
@@ -36,8 +36,12 @@ def parse_sequence_file(file_path):
 
             ss_pred = line
 
+            seq_dict[name] = (sequence, ss_pred)
+
+            """
             if 'U' not in ss_pred:
                 seq_dict[name] = (sequence, ss_pred)
+            """
 
         counter += 1
 
@@ -45,7 +49,11 @@ def parse_sequence_file(file_path):
 
 
 def get_k_gram(strin, k):
-    if k < len(strin):
+    """
+    Compute k-gram of a given sequence
+    """
+
+    if k > len(strin):
 
         return {}
 
@@ -53,7 +61,7 @@ def get_k_gram(strin, k):
 
         kgram = {}
 
-        for i in xrange(len(str) - k):
+        for i in xrange(len(strin) - k + 1):
 
             this_str = strin[i:(i + k)]
 
@@ -70,20 +78,54 @@ def get_k_gram(strin, k):
 
 
 def get_aa_distrib(seq):
+    """
+    Calculate the distribution of
+    amino acid in a given sequence
+    """
 
-    init_distrib = {'a': 0, 'r': 0, 'n': 0,
-					'd': 0, 'c': 0, 'e': 0,
-					'q': 0, 'g': 0, 'h': 0,
-					'i': 0, 'l': 0, 'k': 0,
-					'm': 0, 'f': 0, 'p': 0,
-					's': 0, 't': 0, 'w': 0,
-					'y': 0, 'v': 0}
+    aa_distrib = {'a': 0, 'r': 0, 'n': 0,
+                  'd': 0, 'c': 0, 'e': 0,
+                  'q': 0, 'g': 0, 'h': 0,
+                  'i': 0, 'l': 0, 'k': 0,
+                  'm': 0, 'f': 0, 'p': 0,
+                  's': 0, 't': 0, 'w': 0,
+                  'y': 0, 'v': 0}
 
     for chr in seq:
 
-		init_distrib[chr] = init_distrib[chr] + 1
+        aa_distrib[chr] = aa_distrib[chr] + 1
 
-    return init_distrib
+    return aa_distrib
+
+def merge_non_uniform_dict(d1, d2):
+
+    """
+    Merge the values of two dicts.
+    Unlike merge_dict, it is not assumed
+    that both dicts have identical key set
+    """
+
+    keys = set(d1.keys() + d2.keys())
+
+    merged_dict = {}
+
+    for key in keys:
+
+        if key in d1:
+
+            merged_dict[key] = d1[key]
+
+        if key in d2:
+
+            if key in merged_dict:
+
+                merged_dict[key] = merged_dict[key] + d2[key]
+
+            else:
+
+                merged_dict[key] = d2[key]
+
+    return merged_dict
 
 
 def merge_dict(d1, d2):
@@ -92,19 +134,140 @@ def merge_dict(d1, d2):
     Merge the values of two dicts.
     It is strictly assumed that both dicts have identical keyset
     and the values are of numerical types
-
-    :return: dict with combined values
-
-    :param d1: first dictionary
-    :param d2: second dictionary
-    :return:
     """
 
     for key in d2:
-        d1[key] += d2[key]
 
-	return d1
+        d1[key] = d1[key] + d2[key]
 
+    return d1
+
+def initial_species_statistics(seq_dict, dataset_name):
+    """
+    Get initial statistics for a dataset.
+
+    Data set is represented as dictionary with following structure:
+
+    NAME : (SEQUENCE, SECONDARY_STRUCTURE_ANNOTATION)
+    """
+
+    print "=" * 10
+    print "STATS FOR %s" % dataset_name
+
+    counter = 0
+    len_dict = {}
+    species_dict = {}
+
+    for key in seq_dict:
+        # print key
+        species = (key.split("_")[1]).split()[0]
+        # print species
+        if species in species_dict:
+            species_dict[species] += 1
+        else:
+            species_dict[species] = 1
+
+        this_seq = seq_dict[key][0]
+        len_dict[counter] = len(this_seq)
+        counter += 1
+
+    print "MEAN: " + str(np.mean(len_dict.values()))
+    print "MEDIAN: " + str(np.median(len_dict.values()))
+    print "STD: " + str(np.std(len_dict.values()))
+
+    plt.hist(len_dict.values(), bins=50)
+    plt.title("Sequence Length Histogram")
+    plt.xlabel("Fequency")
+    plt.ylabel("Length")
+    # plt.show()
+    plt.savefig("length_hist_%s.png" % dataset_name, bbox_inches='tight')
+    plt.close()
+
+    for key in species_dict:
+        print "%s\t%s" % (key, str(species_dict[key]))
+
+    print "=" * 10
+
+
+def compute_sequence_distrib(seq_dict, dataset_name):
+    """
+    Compute basic sequence statistics
+    for a given dataset. The statistics computed are:
+    - mean sequence length
+    - median sequence length
+    - std of sequence length
+
+    The function also computes the frequency of
+    UniProt species to appear in the dataset.
+    """
+
+    seq_distrib = None
+
+    for seq_name in seq_dict.keys():
+
+        this_seq = seq_dict[seq_name][0]
+        this_distrib = get_aa_distrib(this_seq.lower())
+
+        if seq_distrib is None:
+
+            seq_distrib = this_distrib
+
+        else:
+
+            seq_distrib = merge_dict(seq_distrib, this_distrib)
+
+    keys = []
+    vals = []
+
+    for key in seq_distrib.keys():
+        keys += [key]
+        vals += [seq_distrib[key]]
+
+    plt.bar(range(len(vals)), vals)
+    plt.xticks(range(len(vals)), keys)
+    plt.title("Amino Acid Distribution of %s Dataset" % dataset_name)
+    plt.xlabel("Amino Acid")
+    plt.ylabel("Frequency")
+    plt.savefig("amino_acid_distrib_%s.png" % dataset_name, bbox_inches='tight')
+    plt.close()
+
+
+def compute_k_gram_statistics(seq_dict, dataset_name, k):
+    """
+    Compute k-gram statistics of data set
+    """
+
+    seq_k_gram = None
+
+    for seq_name in seq_dict:
+
+        this_seq = seq_dict[seq_name][0]
+        this_k_gram = get_k_gram(this_seq, 3)
+
+        if seq_k_gram is None:
+
+            seq_k_gram = this_k_gram
+
+        else:
+
+            seq_k_gram = merge_non_uniform_dict(seq_k_gram, this_k_gram)
+
+    keys = []
+    vals = []
+
+    for key in seq_k_gram.keys():
+        keys += [key]
+        vals += [seq_k_gram[key]]
+
+    plt.hist(vals, bins=len(vals))
+
+    # plt.bar(range(len(vals)), vals)
+    # plt.xticks(range(len(vals)), [])
+    plt.title("Histogram of Sequence %d-Gram for Dataset %s" % (k, dataset_name))
+    plt.ylabel("Frequency")
+    # plt.xlabel("k-Gram")
+    plt.savefig("%d_gram_of_%s.png" % (k, dataset_name), bbox_inches='tight')
+    plt.close()
 
 
 if __name__ == "__main__":
@@ -124,79 +287,30 @@ if __name__ == "__main__":
     opm_seq_dict = parse_sequence_file(opm_path)
 
     # parse PDBTM file and represent it as a map
-    pdbtm_sequence_dict = parse_sequence_file(pdbtm_path)
+    pdbtm_seq_dict = parse_sequence_file(pdbtm_path)
 
-    # print opm_seq_dict
-    # print pdbtm_sequence_dict
+    print opm_seq_dict
+    print pdbtm_seq_dict
 
-    ctr_opm = 0
-    ctr_pdbtm = 0
 
-    ctr_length_opm = 0
-    ctr_length_pdbtm = 0
+    ## Compute basic statistics for both dataset
+    initial_species_statistics(opm_seq_dict, "OPM")
+    initial_species_statistics(pdbtm_seq_dict, "PDBTM")
 
-    opm_len_dict = {}
-    pdbtm_len_dict = {}
 
-    # variables bla and foo can be ignored. they are unimportant because they are only used as intermediate.
-    bla = ""
-    foo = ""
+    ## Compute distribution of amino acid
 
-    # species is powidl. species opm dict / species pdbtm dict are the dicts for species and number of occurence.
-    species = ""
-    species_opm_dict = {}
-    species_pdbtm_dict = {}
-    for key in opm_seq_dict:
-        # print key
-        bla = key.split("_")[1]
-        species = bla.split()[0]
-        # print species
-        if (species in species_opm_dict):
-            species_opm_dict[species] += 1
-        else:
-            species_opm_dict[species] = 1
-        # print species_opm_dict
-        bla = opm_seq_dict[key][0]
-        # print bla
-        opm_len_dict[ctr_opm] = len(bla.split(",")[0])
-        # bla = opm_seq_dict.values()[ctr_opm][0]
-        # opm_len_dict[ctr_opm] = len(bla.split(",")[0])
-        ctr_opm += 1
+    compute_sequence_distrib(opm_seq_dict, "OPM")
+    compute_sequence_distrib(pdbtm_seq_dict, "PDBTM")
 
-    # print "-----------++"
+    print get_k_gram(opm_seq_dict[opm_seq_dict.keys()[0]][0], 3)
 
-    for key2 in pdbtm_sequence_dict:
-        bla = key2.split("_")[1]
-        species = bla.split()[0]
-        # print species
-        if (species in species_pdbtm_dict):
-            species_pdbtm_dict[species] += 1
-        else:
-            species_pdbtm_dict[species] = 1
-        bla = pdbtm_sequence_dict[key2][0]
-        # print bla
-        # bla = pdbtm_sequence_dict.values()[ctr_pdbtm][0]
-        pdbtm_len_dict[ctr_pdbtm] = len(bla.split(",")[0])
-        ctr_pdbtm += 1
+    ## Compute k-gram statistics of dataset
 
-    """
-	Descriptive statistics for the data
-	"""
+    compute_k_gram_statistics(opm_seq_dict, "OPM", 3)
+    compute_k_gram_statistics(pdbtm_seq_dict, "PDBTM", 3)
 
-    print "mean of values in opm database is " + str(np.mean(opm_len_dict.values()))
-    print "median of values in opm database is " + str(np.median(opm_len_dict.values()))
-    print "standard deviation of values in opm database is " + str(np.std(opm_len_dict.values()))
-    print "mean of values in pdbtm database is " + str(np.mean(pdbtm_len_dict.values()))
-    print "median of values in pdbtm database is " + str(np.median(pdbtm_len_dict.values()))
-    print "standard deviation of values in pdbtm database is " + str(np.std(pdbtm_len_dict.values()))
 
-    # plot histogram
-    plt.hist(opm_len_dict.values(), bins=50)
-    plt.title("Sequence lengths in OPM file")
-    plt.xlabel("Length")
-    plt.ylabel("Number of occurences")
-    plt.show()
+    ## TODO Profile and SVM test run
 
-    # print species
-    for i in species_pdbtm_dict:
-        print str(i) + "\t" + str(species_opm_dict[i])
+    print "done"
