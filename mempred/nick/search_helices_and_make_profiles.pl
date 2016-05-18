e139b1ff509db07ca5609634668da0b7cbff626a
nick4git <nickname8@hmamail.com>
2016-05-18 17:40:31 +0200

    25aa -> 20aa
    changed profile lenght

--- mempred/nick/search_helices_and_make_profiles.pl
+++ mempred/nick/search_helices_and_make_profiles.pl
@@ -1,5 +1,5 @@
 
-# searches "*.fasta" (commandline arg) and returns profiles for helices with a length of 25
+# searches "*.fasta" (commandline arg) and returns profiles for helices with a length of 20
 # returns two text files: one for helices & one for non-helices
 
 
@@ -30,7 +30,7 @@ open ($out_nonhelix, '>', 'profiles_nonhelix_'.$commandline_arg.'.txt');
 
 $counter=0;
 $counter_2=0;
-while ($counter<$j){		#main routine: searches for helices with a length > 25 and saves them and the corresponding sequence to a hash
+while ($counter<$j){		#main routine: searches for helices with a length >= 20 and saves them and the corresponding sequence to a hash
 	
 	$stored_line_id='';
 	$stored_line_id=$id_seq_ss{$counter}[0];
@@ -54,7 +54,7 @@ while ($counter<$j){		#main routine: searches for helices with a length > 25 and
 				while (($split_string_into_chars[$i] =~ m/(H|h)/)&& $i<@split_string_into_chars){		#helix
 					$helix_string=$helix_string.'H';
 					$helix_string_seq=$helix_string_seq.$split_string_into_chars_seq[$i];
-					if (length($helix_string)>24){
+					if (length($helix_string)>19){
 						undef $helices{$stored_line_id}{'Helix '.$helix_number};
 						$helices{$stored_line_id}{'Helix '.$helix_number}{$helix_string}=$helix_string_seq;
 					}
@@ -68,7 +68,7 @@ while ($counter<$j){		#main routine: searches for helices with a length > 25 and
 				while (($split_string_into_chars[$i] !~ m/(h|H)/)&& $i<@split_string_into_chars){		#non-helix
 					$nonhelix_string=$nonhelix_string.$split_string_into_chars[$i];
 					$nonhelix_string_seq=$nonhelix_string_seq.$split_string_into_chars_seq[$i];
-					if (length($nonhelix_string)>24){
+					if (length($nonhelix_string)>19){
 						undef $nonhelices{$stored_line_id}{'Non-Helix '.$helix_number};
 						$nonhelices{$stored_line_id}{'Non-Helix '.$nonhelix_number}{$nonhelix_string}=$nonhelix_string_seq;
 					}
@@ -88,10 +88,10 @@ while ($counter<$j){		#main routine: searches for helices with a length > 25 and
 %helices_25=();
 %non_helices_25=();
 
-print $out_helix "ID\tNumber_of_consecutive_helix\tHelix-sequence\tAA-Sequence\n";
-print $out_nonhelix "ID\tNumber_of_consecutive_non-helix\tNon-helix-sequence\tAA-Sequence\n";
+print $out_helix "ID\tNumber_of_consecutive_helix\tAA-Sequence\n";
+print $out_nonhelix "ID\tNumber_of_consecutive_non-helix\tAA-Sequence\n";
 
-foreach my $id (keys %helices) {									#cuts helices at length 25
+foreach my $id (keys %helices) {									#cuts helices at length 20
     foreach my $values (keys %{ $helices{$id} }) {
 		foreach my $ss(keys %{ $helices{$id}{$values} }) {
 			# $random_index = int(rand(length($ss)-25));
@@ -100,24 +100,23 @@ foreach my $id (keys %helices) {									#cuts helices at length 25
 			# $ss_25_seq_h = substr($a, $random_index, 25);
 			# $helices_25{$id}{$values}{$ss_25}=$ss_25_seq_h;
 			# print $out_helix "$id\t$ss_25\t$ss_25_seq_h\n";
-			$rest=(int(length($ss)/25));
-			$mod=(length($ss))%25;
+			$rest=(int(length($ss)/20));
+			$mod=(length($ss))%20;
 			$plus=0;
-			
 			for ($j=0; $j<$rest; $j++){
 				$random_index = int(rand($mod));
-				$ss_25 = substr($ss, $random_index+($plus), 25);
-				$ss_25_seq_h = substr($a, $random_index+($plus), 25);
+				$ss_25 = substr($ss, $random_index+($plus), 20);
+				$ss_25_seq_h = substr($a, $random_index+($plus), 20);
 				$helices_25{$id}{$values}{$j}{$ss_25}=$ss_25_seq_h;
-				print $out_helix "$id\t$j\t$ss_25\t$ss_25_seq_h\n";
-				$plus=25+$random_index+$plus;
+				print $out_helix "$id\t$j\t$ss_25_seq_h\n";
+				$plus=20+$random_index+$plus;
 				$mod=$mod-$random_index;
 			}
 			
 		}
     }
 }
-foreach my $id (keys %nonhelices) {									#cuts non-helices at 25
+foreach my $id (keys %nonhelices) {									#cuts non-helices at 20
     foreach my $values (keys %{ $nonhelices{$id} }) {
 		foreach my $ss (keys %{ $nonhelices{$id}{$values} }) {
 			# $random_index = int(rand(length($ss)-25));
@@ -127,16 +126,16 @@ foreach my $id (keys %nonhelices) {									#cuts non-helices at 25
 			# $nonhelices_25{$id}{$values}{$ss_25_nh}=$ss_25_seq_nh;
 			# print $out_nonhelix "$id\t$ss_25_nh\t$ss_25_seq_nh\n";
 			
-			$rest=(int(length($ss)/25));
-			$mod=(length($ss))%25;
+			$rest=(int(length($ss)/20));
+			$mod=(length($ss))%20;
 			$plus=0;
 			for ($j=0; $j<$rest; $j++){
 				$random_index = int(rand($mod));
-				$ss_25_nh = substr($ss, $random_index+($plus), 25);
-				$ss_25_seq_nh = substr($b, $random_index+($plus), 25);
+				$ss_25_nh = substr($ss, $random_index+($plus), 20);
+				$ss_25_seq_nh = substr($b, $random_index+($plus), 20);
 				$helices_25{$id}{$values}{$j}{$ss_25_nh}=$ss_25_seq_nh;
-				print $out_nonhelix "$id\t$j\t$ss_25_nh\t$ss_25_seq_nh\n";
-				$plus=25+$random_index+$plus;
+				print $out_nonhelix "$id\t$j\t$ss_25_seq_nh\n";
+				$plus=20+$random_index+$plus;
 				$mod=$mod-$random_index;
 			}
 		}
